fbc501f4d0dcec04e37d5ad7dedd4466189a4484
Zenra <dgbaodev2407@gmail.com>
2024-10-14 23:04:59 +0700

    Done ?

--- installer.sh
+++ installer.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "init..."
 echo "N" > .inp
+echo "N" > .inp
+echo "N" > .inp
+echo "N" > .inp
+echo "N" > .inp
+echo "N" > .inp
 echo >> .inp
 pkg update -y && pkg upgrade -y < .inp
-pkg i tsu python3 git apksigner openssl -y
+pkg i tsu python3 git apksigner openssl wget -y < .inp
 #pip install apkutils
 git clone https://github.com/dgbaodev2407/roblox
 cd roblox && sudo python /data/data/com.termux/files/home/roblox/installer.py
