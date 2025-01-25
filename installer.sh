c35f29cfc6832adc814f01111093d55159dfd3f5
dgbaodev2407 <dgbaodev2407@gmail.com>
2025-01-25 20:14:09 +0700

    Roblox 2.655

--- installer.sh
+++ installer.sh
@@ -11,25 +11,19 @@ if [ -e "roblox" ]; then
 	rm -rf roblox
 fi
 git clone https://github.com/dgbaodev2407/roblox
-cd roblox && sudo python /data/data/com.termux/files/home/roblox/installer.py
-if [ $? -eq 0 ]; then
-    echo "OK"
-else
-    echo "Lệnh sudo thất bại."
-    exit 1
-fi
-
-mv roblox/Codex_0.pack roblox/com.roblox.cliena.pack
-mv roblox/Codex_1.pack roblox/com.roblox.clienb.pack
-mv roblox/Codex_2.pack roblox/com.roblox.clienc.pack
-mv roblox/Codex_3.pack roblox/com.roblox.cliend.pack
-mv roblox/Codex_4.pack roblox/com.roblox.cliene.pack
-mv roblox/Codex_5.pack roblox/com.roblox.clienf.pack
-mv roblox/Codex_6.pack roblox/com.roblox.clieng.pack
-mv roblox/Codex_7.pack roblox/com.roblox.clienh.pack
-mv roblox/Codex_8.pack roblox/com.roblox.clieni.pack
-mv roblox/Codex_9.pack roblox/com.roblox.clienj.pack
-mv roblox/Codex_10.pack roblox/com.roblox.clienk.pack
+wget -O roblox/com.roblox.cliena.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Codex_1.pack
+mv roblox/Codex_2.pack roblox/com.roblox.clienb.pack
+mv roblox/Codex_3.pack roblox/com.roblox.clienc.pack
+mv roblox/Codex_4.pack roblox/com.roblox.cliend.pack
+mv roblox/Codex_5.pack roblox/com.roblox.cliene.pack
+mv roblox/Codex_6.pack roblox/com.roblox.clienf.pack
+mv roblox/Codex_7.pack roblox/com.roblox.clieng.pack
+mv roblox/Codex_8.pack roblox/com.roblox.clienh.pack
+mv roblox/Codex_9.pack roblox/com.roblox.clieni.pack
+mv roblox/Codex_10.pack roblox/com.roblox.clienj.pack
+cp roblox/updater.sh /data/data/com.termux/files/usr/bin/updater
+chmod +x /data/data/com.termux/files/usr/bin/updater
+wget "https://raw.githubusercontent.com/hjkbvf/a/refs/heads/main/Rejoin-minh.py" -O roblox/Rejoin.py
 mv roblox /data/data/com.termux/files/usr/roblox
 echo "#!/bin/bash" > /data/data/com.termux/files/usr/bin/clone
 echo "sudo python /data/data/com.termux/files/usr/roblox/manager.py" >> /data/data/com.termux/files/usr/bin/clone
@@ -38,8 +32,11 @@ echo "	echo OK" >> /data/data/com.termux/files/usr/bin/clone
 echo "else" >> /data/data/com.termux/files/usr/bin/clone
 echo "	echo Lệnh sudo thất bại." >> /data/data/com.termux/files/usr/bin/clone
 echo "fi" >> /data/data/com.termux/files/usr/bin/clone
-
 chmod +x /data/data/com.termux/files/usr/bin/clone
-
-echo "done"
-sudo reboot
+cd /data/data/com.termux/files/usr/roblox && python /data/data/com.termux/files/usr/roblox/installer.py
+if [ $? -eq 0 ]; then
+    echo "OK"
+else
+    echo "Lệnh sudo thất bại."
+    exit 1
+fi
--- updater.sh
+++ updater.sh
@@ -3,24 +3,21 @@ cd /data/data/com.termux/files/home/
 if [ -e "roblox" ]; then
 	rm -rf roblox
 fi
-git clone https://github.com/dgbaodev2407/roblox
 rm -rf /data/data/com.termux/files/usr/roblox
-wget -O roblox/Codex.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Codex.pack
-mv roblox/Codex_0.pack roblox/com.roblox.cliena.pack
-mv roblox/Codex_1.pack roblox/com.roblox.clienb.pack
-mv roblox/Codex_2.pack roblox/com.roblox.clienc.pack
-mv roblox/Codex_3.pack roblox/com.roblox.cliend.pack
-mv roblox/Codex_4.pack roblox/com.roblox.cliene.pack
-mv roblox/Codex_5.pack roblox/com.roblox.clienf.pack
-mv roblox/Codex_6.pack roblox/com.roblox.clieng.pack
-mv roblox/Codex_7.pack roblox/com.roblox.clienh.pack
-mv roblox/Codex_8.pack roblox/com.roblox.clieni.pack
-mv roblox/Codex_9.pack roblox/com.roblox.clienj.pack
-mv roblox/Codex_10.pack roblox/com.roblox.clienk.pack
-mv roblox /data/data/com.termux/files/usr/roblox
-sudo python /data/data/com.termux/files/usr/roblox/updater.py 
+git clone https://github.com/dgbaodev2407/roblox
+wget -O roblox/com.roblox.cliena.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Codex_1.pack
+mv roblox/Codex_2.pack roblox/com.roblox.clienb.pack
+mv roblox/Codex_3.pack roblox/com.roblox.clienc.pack
+mv roblox/Codex_4.pack roblox/com.roblox.cliend.pack
+mv roblox/Codex_5.pack roblox/com.roblox.cliene.pack
+mv roblox/Codex_6.pack roblox/com.roblox.clienf.pack
+mv roblox/Codex_7.pack roblox/com.roblox.clieng.pack
+mv roblox/Codex_8.pack roblox/com.roblox.clienh.pack
+mv roblox/Codex_9.pack roblox/com.roblox.clieni.pack
+mv roblox/Codex_10.pack roblox/com.roblox.clienj.pack
 cp roblox/updater.sh /data/data/com.termux/files/usr/bin/updater
-#echo "#!/bin/bash" > /data/data/com.termux/files/usr/bin/updater
-#echo "sudo python /data/data/com.termux/files/usr/roblox/updater.py" >> /data/data/com.termux/files/usr/bin/updater
 chmod +x /data/data/com.termux/files/usr/bin/updater
+wget "https://raw.githubusercontent.com/hjkbvf/a/refs/heads/main/Rejoin-minh.py" -O roblox/Rejoin.py
+mv roblox /data/data/com.termux/files/usr/roblox
+cd /data/data/com.termux/files/usr/roblox && sudo python /data/data/com.termux/files/usr/roblox/updater.py
 echo "Done"
