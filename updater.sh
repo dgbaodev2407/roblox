2d3ed3c5c6cb1e0aa8257db8edd51877ad0c1b4b
Quang Huy <103049818+u400822@users.noreply.github.com>
2025-02-10 14:09:25 +0700

    new

--- updater.sh
+++ updater.sh
@@ -5,16 +5,16 @@ if [ -e "roblox" ]; then
 fi
 rm -rf /data/data/com.termux/files/usr/roblox
 git clone https://github.com/dgbaodev2407/roblox
-wget -O roblox/com.roblox.cliena.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Codex_1.pack
-mv roblox/Codex_2.pack roblox/com.roblox.clienb.pack
-mv roblox/Codex_3.pack roblox/com.roblox.clienc.pack
-mv roblox/Codex_4.pack roblox/com.roblox.cliend.pack
-mv roblox/Codex_5.pack roblox/com.roblox.cliene.pack
-mv roblox/Codex_6.pack roblox/com.roblox.clienf.pack
-mv roblox/Codex_7.pack roblox/com.roblox.clieng.pack
-mv roblox/Codex_8.pack roblox/com.roblox.clienh.pack
-mv roblox/Codex_9.pack roblox/com.roblox.clieni.pack
-mv roblox/Codex_10.pack roblox/com.roblox.clienj.pack
+wget -O roblox/com.roblox.cliena.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Roblox_1.pack
+mv roblox/Roblox_2.pack roblox/com.roblox.clienb.pack
+mv roblox/Roblox_3.pack roblox/com.roblox.clienc.pack
+mv roblox/Roblox_4.pack roblox/com.roblox.cliend.pack
+mv roblox/Roblox_5.pack roblox/com.roblox.cliene.pack
+mv roblox/Roblox_6.pack roblox/com.roblox.clienf.pack
+mv roblox/Roblox_7.pack roblox/com.roblox.clieng.pack
+mv roblox/Roblox_8.pack roblox/com.roblox.clienh.pack
+mv roblox/Roblox_9.pack roblox/com.roblox.clieni.pack
+mv roblox/Roblox_10.pack roblox/com.roblox.clienj.pack
 cp roblox/updater.sh /data/data/com.termux/files/usr/bin/updater
 chmod +x /data/data/com.termux/files/usr/bin/updater
 mv roblox /data/data/com.termux/files/usr/roblox
