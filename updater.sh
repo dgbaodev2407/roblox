3ce5b3742efee47d2c858b7a0dc34702e9a48f1c
Quang Huy <mqh07112008@gmail.com>
2025-04-05 00:46:35 +0700

    ad

--- updater.sh
+++ updater.sh
@@ -8,16 +8,16 @@ if [ -e "/data/data/com.termux/files/usr/roblox" ]; then
 fi
 
 git clone https://github.com/dgbaodev2407/roblox
-wget -O roblox/com.roblox.cliena.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Roblox_1.pack
-mv roblox/Roblox_2.pack roblox/com.roblox.clienb.pack
-mv roblox/Roblox_3.pack roblox/com.roblox.clienc.pack
-mv roblox/Roblox_4.pack roblox/com.roblox.cliend.pack
-mv roblox/Roblox_5.pack roblox/com.roblox.cliene.pack
-mv roblox/Roblox_6.pack roblox/com.roblox.clienf.pack
-mv roblox/Roblox_7.pack roblox/com.roblox.clieng.pack
-mv roblox/Roblox_8.pack roblox/com.roblox.clienh.pack
-mv roblox/Roblox_9.pack roblox/com.roblox.clieni.pack
-mv roblox/Roblox_10.pack roblox/com.roblox.clienj.pack
+wget -O roblox/com.yulgang.ana.pack https://github.com/dgbaodev2407/roblox/releases/download/Roblox-Pack/Roblox_1.pack
+mv roblox/Roblox_2.pack roblox/com.yulgang.anb.pack
+mv roblox/Roblox_3.pack roblox/com.yulgang.anc.pack
+mv roblox/Roblox_4.pack roblox/com.yulgang.and.pack
+mv roblox/Roblox_5.pack roblox/com.yulgang.ane.pack
+mv roblox/Roblox_6.pack roblox/com.yulgang.anf.pack
+mv roblox/Roblox_7.pack roblox/com.yulgang.ang.pack
+mv roblox/Roblox_8.pack roblox/com.yulgang.anh.pack
+mv roblox/Roblox_9.pack roblox/com.yulgang.ani.pack
+mv roblox/Roblox_10.pack roblox/com.yulgang.anj.pack
 cp roblox/updater.sh /data/data/com.termux/files/usr/bin/updater
 chmod +x /data/data/com.termux/files/usr/bin/updater
 mv roblox /data/data/com.termux/files/usr/
