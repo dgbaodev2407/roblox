4aa39ca749d9a4dde9b629dd018fdd3922f9c984
dgbaodev2407 <dgbaodev2407@gmail.com>
2025-05-26 21:16:32 +0700

    add tsu sudo

--- DSU/installer.sh
+++ DSU/installer.sh
@@ -2,19 +2,24 @@
 
 curl -o "$TMPDIR/dsuperuser" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/DSU/dsuperuser
 
+curl -o "$TMPDIR/dsudo" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/DSU/dsudo
+
 mkdir -p "$HOME/bin"
 
 install -Dm755 "$TMPDIR/dsuperuser" "$HOME/bin/dsuperuser"
 
-rm -rf "$HOME/bin/su" "$HOME/bin/sudo"
+install -Dm755 "$TMPDIR/dsudo" "$HOME/bin/sudo"
+
+rm -rf "$HOME/bin/su"
+# rm -rf "$HOME/bin/sudo"
 
 ln -ds "$HOME/bin/dsuperuser" "$HOME/bin/su"
 
-ln -ds "$HOME/bin/dsuperuser" "$HOME/bin/sudo"
-
-echo "export PATH=\"$HOME/bin:$PREFIX/bin:\$PATH\"" >> "$HOME/.bashrc"
+# ln -ds "$HOME/bin/dsuperuser" "$HOME/bin/sudo"
 
 export PATH="$HOME/bin:$PREFIX/bin:$PATH"
+
+echo "export PATH=\"$HOME/bin:$PREFIX/bin:\$PATH\"" >> "$HOME/.bashrc"
 
 # source "$HOME/.bashrc"
 
