452514014dbd23a36a9429520642016c2cedf434
dgbaodev2407 <dgbaodev2407@gmail.com>
2025-05-26 13:19:10 +0700

    update dsuperuser

--- Vmos/installer.sh
+++ Vmos/installer.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-curl -o "$TMPDIR/su" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/Vmos/su
+curl -o "$TMPDIR/dsuperuser" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/Vmos/dsuperuser
 
 mkdir -p "$HOME/bin"
 
-install -Dm755 "$TMPDIR/su" "$HOME/bin/dsuperuser"
+install -Dm755 "$TMPDIR/dsuperuser" "$HOME/bin/dsuperuser"
 
-rm -rf "$HOME/bin/su"
+rm -rf "$HOME/bin/su" "$HOME/bin/sudo"
 
 ln -ds "$HOME/bin/dsuperuser" "$HOME/bin/su"
 
+ln -ds "$HOME/bin/dsuperuser" "$HOME/bin/sudo"
+
 echo "export PATH=\"$HOME/bin:$PREFIX/bin:\$PATH\"" >> "$HOME/.bashrc"
 
-source "$HOME/.bashrc"
+PATH="$HOME/bin:$PATH"
+
+# source "$HOME/.bashrc"
 
 dsuperuser --help
 
 echo
-echo "Cài đặt hoàn tất!"
+echo "Đã cài đặt thành công dsuperuser!"
