fc8c34362105273e0c8ef2419c174bfbeffc60cc
dgbaodev2407 <dgbaodev2407@gmail.com>
2025-05-26 21:27:29 +0700

    fix

--- DSU/installer.sh
+++ DSU/installer.sh
@@ -2,13 +2,13 @@
 
 curl -o "$TMPDIR/dsuperuser" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/DSU/dsuperuser
 
-curl -o "$TMPDIR/dsudo" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/DSU/dsudo
+curl -o "$TMPDIR/tsu-sudo" https://raw.githubusercontent.com/dgbaodev2407/roblox/refs/heads/main/DSU/tsu-sudo
 
 mkdir -p "$HOME/bin"
 
 install -Dm755 "$TMPDIR/dsuperuser" "$HOME/bin/dsuperuser"
 
-install -Dm755 "$TMPDIR/dsudo" "$HOME/bin/sudo"
+install -Dm755 "$TMPDIR/tsu-sudo" "$HOME/bin/sudo"
 
 rm -rf "$HOME/bin/su"
 # rm -rf "$HOME/bin/sudo"
